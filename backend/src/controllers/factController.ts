772c1a11aabc885c29f78e626b8613fcdb2b50a6
MarcPIster <marc.pister@epitech.eu>
2025-02-15 21:07:54 +0100

    add mode to facts

--- backend/src/controllers/factController.ts
+++ backend/src/controllers/factController.ts
@@ -23,10 +23,10 @@ const prompt: string =
     ". Now generate ONE another useless but true fact:";
 
 
-export const saveFact = async (fact: string) => {
+export const saveFact = async (fact: string, mode: number) => {
   try {
     // Create a new Fact document and save it to the DB
-    const newFact = new Fact({ fact });
+    const newFact = new Fact({ fact, mode });
     const savedFact = await newFact.save();
     return savedFact;
   } catch (error) {
@@ -41,6 +41,16 @@ export const getFact = async (factId: number) => {
         return fact;
     } catch (error) {
         console.error('Error fetching fact:', error);
+        return;
+    }
+}
+
+export const getFactsFromMode = async (mode: number) => {
+    try {
+        const facts = await Fact.find({ mode: mode });
+        return facts;
+    } catch (error) {
+        console.error('Error fetching facts:', error);
         return;
     }
 }
@@ -62,32 +72,37 @@ export const generateFact = async (req: Request, res: Response): Promise<void> =
   try {
     console.log('Generating fact...');
     const userId = req.body.userId;
+    const mode = req.body.mode;
     let user;
     if (!userId) {
       user = await createUser();
     } else {
       user = await getUser(userId);
     }
+    if (mode < 0 || mode > 3 || mode === undefined) {
+      res.status(400).json({error: 'Invalid mode'});
+    }
+    const modeFacts = await getFactsFromMode(mode);
+    const randomFactIndex = Math.floor(Math.random() * modeFacts!.length);
+    if (user.factIDs.includes(modeFacts![randomFactIndex].id)) {
+      console.log('User already has fact with id:', modeFacts![randomFactIndex].id);
 
-    const maxFactId = await getMaxFactId();
-    const randomFactId = Math.floor(Math.random() * maxFactId);
-    if (user.factIDs.includes(randomFactId)) {
-      console.log('User already has fact with id:', randomFactId);
       const response = await axios.post(externalURL, {
         model: 'mistral',
         prompt: prompt,
         stream: false,
       });
+
       console.log('External service response:', response.data);
-      const fact = await saveFact(response.data.response);
+      const fact = await saveFact(response.data.response, mode);
       if (!fact) {
         res.status(500).json({error: 'Error saving the fact'});
       }
       user = await updateUser(user.id, fact?.id);
       res.status(200).json({fact: response.data.response, id: user.id});
     } else {
-      console.log('User hasnt save the fact with id:', randomFactId);
-      const fact = await getFact(randomFactId);
+      console.log('User hasnt save the fact with id:', modeFacts![randomFactIndex].id);
+      const fact = await getFact(modeFacts![randomFactIndex].id);
       console.log('Fact from DB:', fact);
       console.log('User:', user);
       user = await updateUser(user.id, fact!.id);
