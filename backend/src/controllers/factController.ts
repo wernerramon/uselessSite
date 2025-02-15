bf98c64582670ddf7520f13fae5646b3cd78449e
MarcPIster <marc.pister@epitech.eu>
2025-02-15 19:02:35 +0100

    change structure to best practise and add mongoDB support

---
+++ backend/src/controllers/factController.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from 'express';
+import axios from 'axios';
+import { Fact } from '../models/factsModel';
+
+// External service configuration
+const externalHostIP = '127.0.0.1';
+const externalURL = `http://${externalHostIP}:11434/api/generate`;
+
+// Examples used for prompt generation
+const examples: string[] = [
+  "A shrimp's heart is located in its head",
+  "Bananas are berries, but strawberries are not",
+  "Wombat poop is cube-shaped",
+  "A day on Venus is longer than a year on Venus",
+  "The inventor of the frisbee was turned into a frisbee after he died",
+];
+
+// Generate a prompt using one random example
+const prompt: string =
+    "Generate a useless but true fact. Example: " +
+    examples[Math.floor(Math.random() * examples.length)] +
+    ". Now generate ONE another useless but true fact:";
+
+
+export const saveFact = async (fact: string) => {
+  try {
+
+
+    // Create a new Fact document and save it to the DB
+    const newFact = new Fact({ fact });
+    const savedFact = await newFact.save();
+  } catch (error) {
+    console.error('Error creating fact:', error);
+  }
+};
+
+/**
+ * Controller to generate a fact from an internal AI.
+ * GET /fact
+ */
+export const generateFact = async (req: Request, res: Response): Promise<void> => {
+  try {
+    console.log('Generating fact...');
+    const response = await axios.post(externalURL, {
+      model: 'mistral',
+      prompt: prompt,
+      stream: false,
+    });
+    console.log('External service response:', response.data);
+    await saveFact(response.data.response);
+    res.status(200).json({ fact: response.data.response });
+  } catch (error) {
+    console.error('Error fetching fact from external service:', error);
+    res.status(500).json({ error: 'Error fetching the fact' });
+  }
+};
